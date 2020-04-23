a0496831cfe5f0b447f50d7a62f1dcb5967c8c97
kattanaveenkumar <katta.naveekumar95@gmail.com>
2020-04-24 00:46:11 +0530

    seeds

--- README.md
+++ README.md
@@ -6,19 +6,8 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
+ruby 2.6.2p47
+Rails 5.2.4.2
 
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+* Db configuration
+run 'rails db:create db:migrate db:seed'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+channel_names = ["HBO", "Star Sports", "ESPN", "National Geographic", "CNN"]
+channel_names.each{|c| Channel.create(name: c)}
+
+Show.create(name: "The Hulk", start_time: "2020-04-24 11:00", channel_id: 1)
+Show.create(name: "India vs Australia Cricket Match", start_time: "2020-04-24 11:00", channel_id: 2)
+Show.create(name: "Soccer Match", start_time: "2020-04-24 11:00", channel_id: 3)
+Show.create(name: "Adventures", start_time: "2020-04-24 11:00", channel_id: 4)
+Show.create(name: "News", start_time: "2020-04-24 11:00", channel_id: 5)
